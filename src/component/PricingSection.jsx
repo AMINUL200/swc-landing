933d07406ee75976a208df522bc6053d1c8e644c
Aminul Islm <ai8728340@gmail.com>
2025-05-19 22:52:45 +0530

    update

--- src/component/PricingSection.jsx
+++ src/component/PricingSection.jsx
@@ -57,7 +57,7 @@ const PricingSection = ({ pricingData, addGap }) => {
                                             <div className="pricing-card-header">
                                                 <h6>{plan.heading1}</h6>
                                                 <div className="price-wrapper">
-                                                    <span className="price">${plan.amount}</span>
+                                                    <span className="price">£ {plan.amount}</span>
                                                     <span className="text"> / Per Month</span>
                                                 </div>
                                                 <p className="text">{plan.description}</p>
